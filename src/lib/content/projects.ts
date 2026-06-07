3d0ebe4a223f619af09170bc5ba57db4b39468e8
Nisha <nishahanamashetty678@gmail.com>
2026-06-07 15:58:02 +0530

    feat: add Task Manager and Student Grade Manager projects to portfolio

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -6,5 +6,24 @@ const currentYear: number = new Date().getFullYear()
 export const projectsSection: ProjectsSectionType = {
     title: 'my projects',
 
-    projects: [],
+    projects: [
+        {
+            id: getId(),
+            name: 'Task Manager App',
+            url: 'https://nisha640.github.io/task-manager-app/',
+            repo: 'https://github.com/Nisha640/task-manager-app',
+            img: '/project-img/new-project.jpg',
+            year: 2025,
+            tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+        },
+        {
+            id: getId(),
+            name: 'Student Grade Management System',
+            url: 'https://github.com/Nisha640/student-grade-manager',
+            repo: 'https://github.com/Nisha640/student-grade-manager',
+            img: '/project-img/new-project.jpg',
+            year: 2025,
+            tags: ['Java', 'JDBC', 'MySQL', 'OOP'],
+        },
+    ],
 };
